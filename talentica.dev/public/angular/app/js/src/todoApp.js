cacd7c2553c1415b9d707cd589df9d79f2040995
Santosh Mishra <stmishra@fastmail.fm>
2014-07-18 11:09:00 +0530

    Angular integration

--- talentica.dev/public/angular/app/js/src/todoApp.js
+++ talentica.dev/public/angular/app/js/src/todoApp.js
@@ -14,8 +14,8 @@ angular.module('todoApp', [
 	  'todoApp.controllers'
 	])
 	.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'todoApp.controllers.login'});
-		$routeProvider.when('/lists', {templateUrl: 'partials/lists.html', controller: 'todoApp.controllers.lists'});
+		$routeProvider.when('/login', {templateUrl: 'angular/app/partials/login.html', controller: 'todoApp.controllers.login'});
+		$routeProvider.when('/lists', {templateUrl: 'angular/app/partials/lists.html', controller: 'todoApp.controllers.lists'});
 		$routeProvider.otherwise({redirectTo: '/login'});
 	}])
 ;
